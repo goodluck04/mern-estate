64c01e8415c7fe4bce49e42fb7bf647740335dcd
goodluck04 <goodluckkumar04@gmail.com>
2023-11-09 13:13:48 +0530

    Connected MongoDB and env file

--- api/index.js
+++ api/index.js
@@ -1,8 +1,18 @@
+import "dotenv/config";
 import express from "express";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-})  
+  console.log("Server is running on port 3000");
+});
