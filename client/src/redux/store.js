d3caa84bd7a054095324d89e3d5950df5ecff23b
goodluck04 <goodluckkumar04@gmail.com>
2023-11-09 17:28:38 +0530

    Persist data using redux-persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,14 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./user/userSlice.js"
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({  
+    getDefaultMiddleware({
       serializableCheck: false,
-      // it will avoid serialized for variables in browser
     }),
 });
+
+export const persistor = persistStore(store);
