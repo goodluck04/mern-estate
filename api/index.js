be8ba8450c166242c968220bbf457277f14a17c0
goodluck04 <goodluckkumar04@gmail.com>
2023-11-09 13:59:36 +0530

    create signUp api

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+
 
 
 mongoose
@@ -15,9 +17,17 @@ mongoose
 
 const app = express();
 
+//  by default we are not allowed send json data to the server
+// so we use middleware to parse json 
+app.use(express.json());
+
+
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
 
