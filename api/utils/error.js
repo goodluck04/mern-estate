be60c108559bfb997acab5eae89e6d254adbaaa2
goodluck04 <goodluckkumar04@gmail.com>
2023-11-09 14:16:30 +0530

    Add error handler midllewares

---
+++ api/utils/error.js
@@ -0,0 +1,8 @@
+export const errorHandler = (statusCode, message) => {
+    // js error constructor manually handling error
+    const error = new Error();
+    error.statusCode = statusCode;
+    error.message = message;
+    return error; // or throw erro
+
+}
