d6163c1f5b3f0f03e989baca3e217c55881c4f55
goodluck04 <goodluckkumar04@gmail.com>
2023-11-24 21:15:24 +0530

    added pre-deployment commit

--- api/index.js
+++ api/index.js
@@ -5,9 +5,7 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
-
-
-
+import path from "path";
 
 mongoose
   .connect(process.env.URL)
@@ -18,17 +16,17 @@ mongoose
     console.log(err);
   });
 
+// dynamic path name for static files and folder of client
+const __dirname = path.resolve();
+
 const app = express();
 
 //  by default we are not allowed send json data to the server
-// so we use middleware to parse json 
+// so we use middleware to parse json
 app.use(express.json());
 
-// 
+// cookie parsing with this
 app.use(cookieParser());
-
-
-
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
@@ -38,16 +36,21 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
-// middle ware for error handling  use as next(error) in catch block
-app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal Server Error";
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message,
-    });
+// static folder of client  (in case creat-react-app dist will be build name instead)
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+// route error handling for unknown routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
-
-
+// middle ware for error handling  use as next(error) in catch block
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
